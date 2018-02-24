795553b83f947a72f7b0d41a097cb109c8d8c8af
baidoosik <qoentlr37@naver.com>
2018-02-24 13:45:55 +0900

    static 서빙 셋팅 추가

--- ohmyeye/settings.py
+++ ohmyeye/settings.py
@@ -119,3 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'ohmyeye', 'static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
